8894f5f8b0fa697354a17d4beb5aa352462ec8c9
Mariano Sheena Dale <smariano@gemalto.com>
2017-12-04 15:25:42 +0800

    Initial commit

---
+++ rot.rb
@@ -0,0 +1,44 @@
+class ROT
+  attr_accessor :places
+  
+  def decrypt(message)
+    decrypted_msg = Array.new
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+    if places.zero? or message == "\n"
+      return ""
+    end
+    
+    msg_array = message.strip.downcase.split(//)
+    for letter in msg_array
+      if letters.include?(letter)
+        new_index = letters.index(letter) + places
+        
+        if new_index > (letters.size - 1)
+          new_index = letters.size - new_index
+        end
+        
+        decrypted_msg.push(letters[new_index])
+      elsif
+        decrypted_msg.push(letter)
+      end
+    end
+    
+    return decrypted_msg.join
+  end
+end
+
+cipher = ROT.new
+
+puts "Enter ROT places: "
+cipher.places = gets.to_i
+
+puts "Enter message: "
+message = gets
+
+puts ""
+printf "ROT places: %d", cipher.places
+puts ""
+printf "Encrypted message: %s", message
+puts ""
+printf "Decrypted message: %s", cipher.decrypt(message)
